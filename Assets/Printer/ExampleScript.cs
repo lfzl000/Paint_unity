48a17edf7e958e5ed4eb2f1b154be68d451527d1
zhaoliang <zhaoliang@bimsop.com>
2017-07-28 12:12:33 +0800

    init

---
+++ Assets/Printer/ExampleScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+using System;
+using System.IO;
+using UnityEngine.UI;
+
+public class ExampleScript : MonoBehaviour
+{
+    public Texture2D texture2D;
+    public string printerName = "";     //打印机名称，如果为空使用默认打印机
+    public int copies = 1;              //打印份数
+
+    public InputField inputField;
+
+    public void PrintTextureButton()
+    {
+        Print.PrintTexture(texture2D.EncodeToPNG(), copies, printerName);
+    }
+
+    public void PrintByPathButton()
+    {
+        Print.PrintTextureByPath(inputField.text.Trim(), copies, printerName);
+    }
+}
---
+++ Assets/Scripts/Print.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing.Printing;
+using System.IO;
+using UnityEngine;
+
+public static class Print
+{
+    public static void PrintTexture(byte[] texture2DBytes, int numCopies, string printerName)
+    {
+        if (texture2DBytes == null)
+        {
+            UnityEngine.Debug.LogWarning("Texture is empty.");
+            return;
+        }
+        PrinterSettings printerSettings = new PrinterSettings();
+        if (printerName == null || printerName.Equals(""))
+        {
+            printerName = printerSettings.PrinterName;
+            UnityEngine.Debug.Log("Printing to default printer (" + printerName + ").");
+        }
+        string str = string.Concat(new string[]
+        {
+                DateTime.Now.Year.ToString(),
+                "-",
+                DateTime.Now.Month.ToString(),
+                "-",
+                DateTime.Now.Day.ToString(),
+                "-",
+                DateTime.Now.Hour.ToString(),
+                "-",
+                DateTime.Now.Minute.ToString(),
+                "-",
+                DateTime.Now.Second.ToString(),
+                "-",
+                DateTime.Now.Millisecond.ToString()
+        });
+        string text = (Application.persistentDataPath + "\\PrinterFiletmp_" + str + ".png").Replace("/", "\\");
+        UnityEngine.Debug.Log("Temporary Path - " + text);
+        File.WriteAllBytes(text, texture2DBytes);
+        Print.PrintCMD(text, numCopies, printerName);
+    }
+
+    public static void PrintTextureByPath(string path, int numCopies, string printerName)
+    {
+        PrinterSettings printerSettings = new PrinterSettings();
+        if (printerName == null || printerName.Equals(""))
+        {
+            printerName = printerSettings.PrinterName;
+            UnityEngine.Debug.Log("Printing to default printer (" + printerName + ").");
+        }
+        Print.PrintCMD(path, numCopies, printerName);
+    }
+
+    private static void PrintCMD(string path, int numCopies, string printerName)
+    {
+        Process process = new Process();
+        try
+        {
+            for (int i = 0; i < numCopies; i++)
+            {
+                process.StartInfo.FileName = "rundll32";
+                process.StartInfo.Arguments = string.Concat(new string[]
+                {
+                        "C:\\WINDOWS\\system32\\shimgvw.dll,ImageView_PrintTo \"",
+                        path,
+                        "\" \"",
+                        printerName,
+                        "\""
+                });
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                process.StartInfo.UseShellExecute = true;
+                process.Start();
+            }
+        }
+        catch (Exception arg)
+        {
+            UnityEngine.Debug.LogError(arg);
+        }
+        finally
+        {
+            process.Close();
+            UnityEngine.Debug.Log("Texture printing.");
+        }
+    }
+}
